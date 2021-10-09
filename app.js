09192cd499fc7d741a516e8fc1cdf457b6fc956e
kazarixx <tua.ih8@gmail.com>
2021-10-10 03:05:45 +0900

    [finish]6章

--- app.js
+++ app.js
@@ -1,30 +1,8 @@
-// $(document).ready(function () {
-  // $('body').html('<h1>Hello jQuery!!</h1>');
-// });
-
-// $(function(){
-//  $( セレクタ ).メソッド ( パラメータ );
-// }); 
-
-// 「セレクタ」内のメソッドを、パラーメーターで書き換える
-
-// // 　「.box1」内のCSSを書き換える
-// $(function(){
-//   $('.box1').css({
-//     'background-color': '#0000FF',
-//     'height': '100px'
-//   });
-// });
-
-// /2.1 上から下へスライドさせる/ 
-// 「.box1」をslidDown
 $(function(){
-  $('.box1').slideDown(function(){
-   $('.box1').css({
-    'background-color':'blue',
-    'height':'100px',
-    'width':'200px'
-   })
-   $('.box1').slideUp();
+  $('.box1').on('click', function(){
+    $('.box1').addClass('box1-ext');
+  });
+  $('.box1').mouseout(function(){
+    $('.box1').removeClass('box1-ext');
   });
 });
